5b4ab39a2f6684f1866c1d29262b3ba03cae11d8
jeetpal000 <chandrajeetcare2019@gmail.com>
2024-10-17 15:15:51 +0530

    DOM-Project

---
+++ script.js
@@ -0,0 +1,11 @@
+let divElement = document.getElementById("box");
+console.log("divElement");
+
+divElement.style.color = "#022";
+divElement.style.backgroundColor = "cyan";
+divElement.style.margin = "20px";
+divElement.style.padding = "10px";
+divElement.style.fontSize = "18px";
+divElement.style.fontWeight = "bold";
+divElement.style.height = "200px";
+divElement.style.width = "300px";
